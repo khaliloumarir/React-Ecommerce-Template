80db56ed83a82af82ef8cf90e7e74a7a56c3d8de
DadiT <HackerSantochi@gmail.com>
2023-03-21 11:53:35 +0000

    added firebase deployment on push and changed total

--- src/components/ProductBody.tsx
+++ src/components/ProductBody.tsx
@@ -108,7 +108,7 @@ export default function ProductBody({
             Sale could End any minute! Get your PDF Now before it is too late
           </p> */}
           <p className="text-greenColor font-bold">
-            {productPrice}$ - In stock (104 Sold)
+            {productPrice}$ - In stock (109 Sold)
           </p>
         </section>
 
