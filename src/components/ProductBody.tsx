3f87da05e449b8ad9afb7d224c25190888b7f6bd
DadiT <76049051+Dadi-T@users.noreply.github.com>
2023-03-30 10:51:49 +0000

    changed numbers of sold

--- src/components/ProductBody.tsx
+++ src/components/ProductBody.tsx
@@ -134,7 +134,7 @@ export default function ProductBody({
             Sale could End any minute! Get your PDF Now before it is too late
           </p> */}
           <p className="text-greenColor font-bold">
-            {productPrice}$ - In stock (115 Sold)
+            {productPrice}$ - In stock (120 Sold)
           </p>
         </section>
 
