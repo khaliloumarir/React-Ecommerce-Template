80ab2591372e924957e324e944b758e626de879f
DadiT <76049051+Dadi-T@users.noreply.github.com>
2023-04-17 12:57:04 +0000

    updated amount sold

--- src/components/ProductBody.tsx
+++ src/components/ProductBody.tsx
@@ -143,7 +143,7 @@ export default function ProductBody({
             Sale could End any minute! Get your PDF Now before it is too late
           </p> */}
           <p className="text-greenColor font-bold">
-            {productPrice}$ - In stock (150 Sold)
+            {productPrice}$ - In stock (153 Sold)
           </p>
         </section>
 
