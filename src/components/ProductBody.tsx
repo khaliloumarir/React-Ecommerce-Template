af9eb049fa1df39bcc5255317f858ef8ddbe727b
DadiT <76049051+Dadi-T@users.noreply.github.com>
2023-05-07 10:22:57 +0100

    Update ProductBody.tsx

--- src/components/ProductBody.tsx
+++ src/components/ProductBody.tsx
@@ -146,7 +146,7 @@ export default function ProductBody({
             Sale could End any minute! Get your PDF Now before it is too late
           </p> */}
           <p className="text-greenColor font-bold">
-            {productPrice}$ - In stock (185 Sold)
+            {productPrice}$ - In stock (188 Sold)
           </p>
         </section>
 
